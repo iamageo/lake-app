0d927f890f78c5b00c01826f32de063ed7620f58
iamageo <geovani.af4@gmail.com>
2021-03-09 01:30:31 -0300

    starting login page design

--- app/src/main/java/com/iamageo/lake_app/MainActivity.java
+++ app/src/main/java/com/iamageo/lake_app/MainActivity.java
@@ -2,18 +2,47 @@ package com.iamageo.lake_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn_sign_in;
+    private Button btn_sign_up;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initComponents();
+
+        btn_sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
+
+        btn_sign_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
     }
 
 
-
+    public void initComponents() {
+        btn_sign_in = findViewById(R.id.login_btn);
+        btn_sign_up = findViewById(R.id.register_btn);
+    }
 
 
 
