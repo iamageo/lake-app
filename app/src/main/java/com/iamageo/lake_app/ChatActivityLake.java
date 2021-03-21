002e73a8eeb96e4fcaaff2d6bde9b4a4df202743
iamageo <geovani.af4@gmail.com>
2021-03-21 16:50:32 -0300

    method of sending message working

--- app/src/main/java/com/iamageo/lake_app/ChatActivityLake.java
+++ app/src/main/java/com/iamageo/lake_app/ChatActivityLake.java
@@ -4,34 +4,51 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.KeyEvent;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.TextView;
 
-import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.Objects;
 
 public class ChatActivityLake extends AppCompatActivity {
 
+
+    /* variables input */
     private String mDisplayName;
     private ImageButton mSendButton;
     private EditText mInputText;
-    private ListView mChatListView;
+    //private ListView mChatListView;
+
+    /* firebase */
+    private DatabaseReference mDatabaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_chat);
 
+        setupDisplayName();
+
         /* active action bar */
-        Objects.requireNonNull(getSupportActionBar()).setTitle("");
+        Objects.requireNonNull(getSupportActionBar()).setTitle(mDisplayName);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference();
 
         initComponents();
 
-        setupDisplayName();
+
+        mInputText.setOnEditorActionListener((v, actionId, event) -> {
+            sendMessage();
+            return true;
+        });
+
+        mSendButton.setOnClickListener(v -> sendMessage());
 
     }
 
@@ -42,21 +59,24 @@ public class ChatActivityLake extends AppCompatActivity {
         mSendButton = findViewById(R.id.sendButton);
         mInputText = findViewById(R.id.messageInput);
         mChatListView = findViewById(R.id.chat_list_view);
-
-
     }
 
     //TODO: Retrieve the name from the shared preferences
     private void setupDisplayName() {
         SharedPreferences prefs = getSharedPreferences(RegisterActivity.CHAT_PREFS, MODE_PRIVATE);
         mDisplayName = prefs.getString(RegisterActivity.DISPLAY_NAME_KEY, null);
-
-        if(mDisplayName == null) mDisplayName = "Anonymus";
-
+        if(mDisplayName == null) mDisplayName = "anonymous";
     }
 
     //TODO: SendMessage Method
     private void sendMessage() {
+        String input = mInputText.getText().toString();
+
+        if(!input.equals("")) {
+            InstantMessage chat = new InstantMessage(input, mDisplayName);
+            mDatabaseReference.child("messages").push().setValue(chat);
+            mInputText.setText("");
+        }
 
     }
 }
